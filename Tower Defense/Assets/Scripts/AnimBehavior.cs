84e1754dbdad8a655b774eb1cd497639db0920c5
Emma Gillespie <emma7gillespie@gmail.com>
2020-01-25 12:39:50 -0700

    Animation state practice

---
+++ Tower Defense/Assets/Scripts/AnimBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimBehavior : StateMachineBehaviour{
+
+public UnityEvent stateEnterEvent;
+
+    override public void onStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+      
+    {
+        stateEneterEvent.Invoke();
+    }
+   
+}
---
+++ Tower Defense/Assets/Scripts/DebugObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugObject : MonoBehaviour
+{
+    public void Ondebug (string message)
+    {
+        DebugObject.Log(message);
+    }
+}
+
+
+public class GameAction : ScriptableObject
+{
+
+    public UnityAction action;
+
+    public void Raise()
+    {
+        GameAction?.Invoke();
+    }
+
+}
